42abb6aae5f7864a10196d4c998b1145696bae62
Rj Bernaldo <rj.bernaldo@gmail.com>
2016-02-23 20:35:28 +0800

    Add initial specs for main 'Parent' component

---
+++ client/__tests__/components/FeatureRequestApp.react.spec.js
@@ -0,0 +1,8 @@
+jest.dontMock('../../components/FeatureRequestApp.react');
+
+describe('FeatureRequestApp', function() {
+	it('should render NewFeatureRequest');
+	it('should render ClientList');
+	it('should render ProductAreaList');
+	it('should render FeatureRequestList');
+});
