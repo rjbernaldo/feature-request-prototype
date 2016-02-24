97f973167c39cf690ae55490a4a52135b2a45bab
Rj Bernaldo <rj.bernaldo@gmail.com>
2016-02-24 15:25:30 +0800

    Refactor modal show/hide process

--- client/__tests__/components/NewFeatureRequest.react.spec.js
+++ client/__tests__/components/NewFeatureRequest.react.spec.js
@@ -7,27 +7,15 @@ describe('NewFeatureRequest', function() {
 	var TestUtils = require('react-addons-test-utils');
 
 	var NewFeatureRequest = require('../../components/NewFeatureRequest.react');
-	var NewFeatureRequestModal = require('../../components/NewFeatureRequestModal.react');
-
 	var newFeatureRequest = TestUtils.renderIntoDocument(<NewFeatureRequest />);
 
 	it('should render succesfully', function() {
 		expect(TestUtils.isCompositeComponent(newFeatureRequest)).toBeTruthy();
 	});
 
-	it('should not render modal by default', function() {
-		var newFeatureRequestModal = TestUtils.scryRenderedComponentsWithType(newFeatureRequest, NewFeatureRequestModal);
+	it('should not render modal by default');
 
-		expect(newFeatureRequestModal.length).toBeFalsy();
-	});
+	it('should render modal on click');
 
-	it('should render modal on click', function() {
-		var newFeatureRequestButton = TestUtils.scryRenderedDOMComponentsWithClass(newFeatureRequest, 'new-feature-request-button');
-
-		TestUtils.Simulate.click(newFeatureRequestButton[0]);
-
-		var newFeatureRequestModal = TestUtils.scryRenderedComponentsWithType(newFeatureRequest, NewFeatureRequestModal);
-
-		expect(newFeatureRequestModal.length).toBeTruthy();
-	});
+	it('should close modal')
 });
--- client/components/NewFeatureRequest.react.js
+++ client/components/NewFeatureRequest.react.js
@@ -1,22 +1,43 @@
 var React = require('react');
-var NewFeatureRequestModal = require('./NewFeatureRequestModal.react');
+var Modal = require('react-bootstrap').Modal;
+var Button = require('react-bootstrap').Button;
 
 var NewFeatureRequest = React.createClass({
 	getInitialState: function() {
 		return {
-			modalIsOpen: false
+			showModal: false
 		}
 	},
-	toggleModal: function() {
+	onOpenModal: function() {
 		this.setState({
-			modalIsOpen: !this.state.modalIsOpen
+			showModal: true
 		});
+	},
+	onCloseModal: function() {
+		this.setState({
+			showModal: false
+		});
+	},
+	onSubmit: function() {
+
 	},
 	render: function() {
 		return (
 			<div>
-				<button className="new-feature-request-button" onClick={ this.toggleModal }>New Feature Request</button>
-				{ this.state.modalIsOpen ? <NewFeatureRequestModal /> : null }
+				<button className="new-feature-request-button" onClick={ this.onOpenModal }>New Feature Request</button>
+
+				<Modal show={ this.state.showModal } onHide={ this.onCloseModal } className="static-modal">
+					<Modal.Header>
+						New Feature Request
+					</Modal.Header>
+					<Modal.Body>
+						<input></input>
+					</Modal.Body>
+					<Modal.Footer>
+						<Button onClick={ this.onCloseModal }>Close</Button>
+						<Button bStyle="primary" onClick={ this.onSubmit }>Save</Button>
+					</Modal.Footer>
+				</Modal>
 			</div>
 		);
 	}
